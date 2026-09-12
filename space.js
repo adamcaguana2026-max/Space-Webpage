7c519e8bab1c7ab99cfd4bb1f84b247e7360173e
Adam Caguana <adamcaguana2026@gmail.com>
2026-09-11 20:01:01 -0400

    Added one new image moved the dawrf planet images added a light hover effect moved table and bordered all paragraphs 9/11/26

---
+++ space.js
@@ -0,0 +1,26 @@
+var person = "Friend";
+console.log("Hello " + person + "!");  
+var greeting = "Welcome to my website!";
+console.log(greeting);
+person  = "Explorer";
+greeting = "Explore the wonders of my website, " + person + "!";
+console.log(greeting);
+person = "Visitor";
+console.log("Enjoy your stay, " + person + "!");
+console.log("Thank you for visiting my website, " + person + "!");
+console.log(3*3*9*3);
+console.log(49/24);
+console.log(4/10 == 2/5);
+var result= 1599;
+if (result > 1600) {
+    console.log("HOW DID YOU EVEN MANGE TO GET THIS HIGH OF A SCORE, THATS NOT EVEN POSSIBLE?" );
+} else if (result == 1600) {
+    console.log("You scored exactly 1600, which is the highest possible score on the SAT! You are a genius and can apply to MIT!");
+} else if (result > 1000) {
+    console.log("Congratulations! You can apply to MIT!");
+} else {
+    console.log("Sorry, you cannot apply to MIT, better luck next time on the SAT!");
+}
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
